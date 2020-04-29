132febf1ce69ea1b8bf9079dc7a7701d6f7007e4
R1220 <raffaele.1220@hotmail.com>
2020-04-29 17:06:15 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #FreeDNS updater script
 
-UPDATEURL="http://freedns.afraid.org/dynamic/update.php?T3Z4NXcxaUJqdnYyV0I5cWtHMzhCMjdPOjE4OTA2NDgx"
+UPDATEURL="http://freedns.afraid.org/dynamic/update.php?xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 DOMAIN="paranutti.mooo.com"
 
 registered=$(nslookup $DOMAIN|tail -n2|grep A|sed s/[^0-9.]//g)
